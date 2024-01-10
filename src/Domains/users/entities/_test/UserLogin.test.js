6744bb340d9114694d9353a4b9bc412d4380fed6
Zidan Abraham <zidangit07@gmail.com>
2024-01-10 22:44:24 +0800

    feat(domain/users): add UserLogin entity

---
+++ src/Domains/users/entities/_test/UserLogin.test.js
@@ -0,0 +1,41 @@
+const UserLogin = require('../UserLogin');
+
+describe('UserLogin entity', () => {
+  it('should throw an error when the payload does not contain the required properties', () => {
+    // Arrange
+    const payload = {
+      username: 'danzbraham',
+    };
+
+    // Action and Assert
+    expect(() => new UserLogin(payload)).toThrow('USER_LOGIN.NOT_CONTAIN_REQUIRED_PROPERTY');
+  });
+
+  it('should throw an error when the payload does not meet the data type specification', () => {
+    // Arrange
+    const payload = {
+      username: 'danzbraham',
+      password: 123456,
+    };
+
+    // Action and Assert
+    expect(() => new UserLogin(payload)).toThrow('USER_LOGIN.NOT_MEET_DATA_TYPE_SPECIFICATION');
+  });
+
+  it('should create UserLogin entity correctly', () => {
+    // Arrange
+    const payload = {
+      username: 'danzbraham',
+      password: '123456',
+    };
+
+    // Action
+
+    const userLogin = new UserLogin(payload);
+
+    // Assert
+    expect(userLogin).toBeInstanceOf(UserLogin);
+    expect(userLogin.username).toEqual(payload.username);
+    expect(userLogin.password).toEqual(payload.password);
+  });
+});
