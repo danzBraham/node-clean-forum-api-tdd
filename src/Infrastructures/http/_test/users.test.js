86c578b731985bba2e53d89059f8b9311cbd7730
Zidan Abraham <zidangit07@gmail.com>
2024-01-10 11:50:09 +0800

    feat(infra/interfaces): implement HTTP server and functional tests

---
+++ src/Infrastructures/http/_test/users.test.js
@@ -0,0 +1,39 @@
+const pool = require('../../database/postgres/pool');
+const UsersTableTestHelper = require('../../../../tests/UsersTableTestHelper');
+const container = require('../../container');
+const createServer = require('../createServer');
+
+describe('/users endpoint', () => {
+  afterEach(async () => {
+    await UsersTableTestHelper.cleanTable();
+  });
+
+  afterAll(async () => {
+    await pool.end();
+  });
+
+  describe('when POST /users', () => {
+    it('should response 201 and persist user', async () => {
+      // Arrange
+      const requestPayload = {
+        username: 'danzbraham',
+        password: 'secret',
+        fullname: 'Zidan Abraham',
+      };
+      const server = await createServer(container);
+
+      // Action
+      const response = await server.inject({
+        method: 'POST',
+        url: '/users',
+        payload: requestPayload,
+      });
+
+      // Assert
+      const responseJson = JSON.parse(response.payload);
+      expect(response.statusCode).toEqual(201);
+      expect(responseJson.status).toEqual('success');
+      expect(responseJson.data.addedUser).toBeDefined();
+    });
+  });
+});
