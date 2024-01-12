271291b6150c6c4c1fa8ddcad36129a8b4e5c7ac
Zidan Abraham <zidangit07@gmail.com>
2024-01-12 10:28:59 +0800

    feat(infra): define service locator for UserLoginUseCase

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -4,16 +4,22 @@ const { createContainer } = require('instances-container');
 // external agency
 const { nanoid } = require('nanoid');
 const bcrypt = require('bcrypt');
+const Jwt = require('@hapi/jwt');
 const pool = require('./database/postgres/pool');
 
 // service (repository, helper, manager, etc)
 const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
+const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
 const BcryptPasswordHasher = require('./security/BcryptPasswordHasher');
+const JwtTokenManager = require('./security/JwtTokenManager');
 
 // use case
 const AddUserUseCase = require('../Applications/use_case/AddUserUseCase');
+const UserLoginUseCase = require('../Applications/use_case/UserLoginUseCase');
 const PasswordHasher = require('../Applications/security/PasswordHasher');
+const AuthenticationTokenManager = require('../Applications/security/AuthenticationTokenManager');
 const UserRepository = require('../Domains/users/UserRepository');
+const AuthenticationRepository = require('../Domains/authentications/AuthenticationRepository');
 
 // creating container
 const container = createContainer();
@@ -31,11 +37,29 @@ container.register([
     },
   },
   {
+    key: AuthenticationRepository.name,
+    Class: AuthenticationRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        { concrete: pool },
+      ],
+    },
+  },
+  {
     key: PasswordHasher.name,
     Class: BcryptPasswordHasher,
     parameter: {
       dependencies: [
         { concrete: bcrypt },
+      ],
+    },
+  },
+  {
+    key: AuthenticationTokenManager.name,
+    Class: JwtTokenManager,
+    parameter: {
+      dependencies: [
+        { concrete: Jwt.token },
       ],
     },
   },
@@ -50,6 +74,19 @@ container.register([
       injectType: 'destructuring',
       dependencies: [
         { name: 'userRepository', internal: UserRepository.name },
+        { name: 'passwordHasher', internal: PasswordHasher.name },
+      ],
+    },
+  },
+  {
+    key: UserLoginUseCase.name,
+    Class: UserLoginUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        { name: 'userRepository', internal: UserRepository.name },
+        { name: 'authenticationRepository', internal: AuthenticationRepository.name },
+        { name: 'authenticationTokenManager', internal: AuthenticationTokenManager.name },
         { name: 'passwordHasher', internal: PasswordHasher.name },
       ],
     },
