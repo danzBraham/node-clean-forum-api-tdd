a7cdb4f3ffc8e6b89c8acf51b23f1377b3f7bc0b
Zidan Abraham <zidangit07@gmail.com>
2024-01-12 08:39:54 +0800

    feat(infrastructure): implement JwtTokenManager

---
+++ src/Infrastructures/security/JwtTokenManager.js
@@ -0,0 +1,33 @@
+const AuthenticationTokenManager = require('../../Applications/security/AuthenticationTokenManager');
+const InvariantError = require('../../Commons/InvariantError');
+
+class JwtTokenManager extends AuthenticationTokenManager {
+  constructor(jwt) {
+    super();
+    this._jwt = jwt;
+  }
+
+  async createAccessToken(payload) {
+    return this._jwt.generate(payload, process.env.ACCESS_TOKEN_KEY);
+  }
+
+  async createRefreshToken(payload) {
+    return this._jwt.generate(payload, process.env.REFRESH_TOKEN_KEY);
+  }
+
+  async decodePayload(token) {
+    const artifacts = await this._jwt.decode(token);
+    return artifacts.decoded.payload;
+  }
+
+  async verifyRefreshToken(token) {
+    try {
+      const artifacts = await this._jwt.decode(token);
+      await this._jwt.verify(artifacts, process.env.REFRESH_TOKEN_KEY);
+    } catch (error) {
+      throw new InvariantError('Invalid refresh token');
+    }
+  }
+}
+
+module.exports = JwtTokenManager;
---
+++ src/Infrastructures/security/_test/JwtTokenManager.test.js
@@ -0,0 +1,110 @@
+const Jwt = require('@hapi/jwt');
+const InvariantError = require('../../../Commons/InvariantError');
+const JwtTokenManager = require('../JwtTokenManager');
+
+describe('JwtTokenManager', () => {
+  describe('createAccessToken', () => {
+    it('should create access token correctly', async () => {
+      // Arrange
+      const payload = {
+        id: 'user-123',
+        username: 'danzbraham',
+      };
+      const mockJwtToken = {
+        generate: jest.fn().mockImplementation(() => Promise.resolve('mock-token')),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+
+      // Action
+      const accessToken = await jwtTokenManager.createAccessToken(payload);
+
+      // Assert
+      expect(accessToken).toEqual('mock-token');
+      expect(mockJwtToken.generate).toHaveBeenCalledWith(payload, process.env.ACCESS_TOKEN_KEY);
+    });
+  });
+
+  describe('createRefreshToken', () => {
+    it('should create refresh token correctly', async () => {
+      // Arrange
+      const payload = {
+        id: 'user-123',
+        username: 'danzbraham',
+      };
+      const mockJwtToken = {
+        generate: jest.fn().mockImplementation(() => Promise.resolve('mock-token')),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+
+      // Action
+      const refreshToken = await jwtTokenManager.createRefreshToken(payload);
+
+      // Assert
+      expect(refreshToken).toEqual('mock-token');
+      expect(mockJwtToken.generate).toHaveBeenCalledWith(payload, process.env.REFRESH_TOKEN_KEY);
+    });
+  });
+
+  describe('decodePayload', () => {
+    it('should decode payload correctly', async () => {
+      // Arrange
+      const payload = {
+        id: 'user-123',
+        username: 'danzbraham',
+      };
+      const accessToken = 'access-token';
+      const mockJwtToken = {
+        decode: jest.fn().mockImplementation(() => Promise.resolve({ decoded: { payload } })),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+
+      // Action
+      const decodedPayload = await jwtTokenManager.decodePayload(accessToken);
+
+      // Assert
+      expect(decodedPayload.id).toEqual(payload.id);
+      expect(decodedPayload.username).toEqual(payload.username);
+      expect(mockJwtToken.decode).toHaveBeenCalledWith(accessToken);
+    });
+  });
+
+  describe('verifyRefreshToken', () => {
+    it('should throw an InvariantError when refresh token is invalid', async () => {
+      // Arrange
+      const payload = {
+        id: 'user-123',
+        username: 'danzbraham',
+      };
+      const refreshToken = 'refresh-token';
+      const mockJwtToken = {
+        decode: jest.fn().mockImplementation(() => Promise.resolve({ decoded: { payload } })),
+        verify: jest.fn().mockImplementation(() => Promise.reject()),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+
+      // Action & Assert
+      await expect(jwtTokenManager.verifyRefreshToken(refreshToken))
+        .rejects
+        .toThrow(InvariantError);
+    });
+
+    it('should not throw an InvariantError when refresh token is valid', async () => {
+      // Arrange
+      const payload = {
+        id: 'user-123',
+        username: 'danzbraham',
+      };
+      const refreshToken = 'refresh-token';
+      const mockJwtToken = {
+        decode: jest.fn().mockImplementation(() => Promise.resolve({ decoded: { payload } })),
+        verify: jest.fn().mockImplementation(() => Promise.resolve()),
+      };
+      const jwtTokenManager = new JwtTokenManager(mockJwtToken);
+
+      // Action & Assert
+      await expect(jwtTokenManager.verifyRefreshToken(refreshToken))
+        .resolves
+        .not.toThrow(InvariantError);
+    });
+  });
+});
