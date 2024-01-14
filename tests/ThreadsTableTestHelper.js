3e65cbddaafcc35cf879c0f424f102f7e0cdd26e
Zidan Abraham <zidangit07@gmail.com>
2024-01-14 21:27:52 +0800

    feat(tests): introduce ThreadsTableTestHelper for testing support

---
+++ tests/ThreadsTableTestHelper.js
@@ -0,0 +1,32 @@
+// istanbul ignore file
+const pool = require('../src/Infrastructures/database/postgres/pool');
+
+const ThreadsTableHelper = {
+  async addThread({
+    id = 'thread-123', title = 'My Thread', body = 'Hello this is my Thread', owner = 'user-123',
+  }) {
+    const date = new Date().toISOString();
+    const query = {
+      text: 'INSERT INTO threads VALUES ($1, $2, $3, $4, $5)',
+      values: [id, title, body, date, owner],
+    };
+
+    await pool.query(query);
+  },
+
+  async findThreadById(id) {
+    const query = {
+      text: 'SELECT * FROM threads WHERE id = $1',
+      values: [id],
+    };
+
+    const { rows } = await pool.query(query);
+    return rows;
+  },
+
+  async cleanTable() {
+    await pool.query('DELETE FROM threads WHERE 1 = 1');
+  },
+};
+
+module.exports = ThreadsTableHelper;
