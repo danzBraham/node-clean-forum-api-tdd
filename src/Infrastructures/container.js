d2381f8b3a3c9351027fa0f8774817a46ebdfbca
Zidan Abraham <zidangit07@gmail.com>
2024-01-15 15:53:03 +0800

    feat(infra): define service locator for AddCommentUseCase

--- src/Infrastructures/container.js
+++ src/Infrastructures/container.js
@@ -11,6 +11,7 @@ const pool = require('./database/postgres/pool');
 const UserRepositoryPostgres = require('./repository/UserRepositoryPostgres');
 const AuthenticationRepositoryPostgres = require('./repository/AuthenticationRepositoryPostgres');
 const ThreadRepositoryPostgres = require('./repository/ThreadRepositoryPostgres');
+const CommentRepositoryPostgres = require('./repository/CommentRepositoryPostgres');
 const BcryptPasswordHasher = require('./security/BcryptPasswordHasher');
 const JwtTokenManager = require('./security/JwtTokenManager');
 
@@ -18,6 +19,7 @@ const JwtTokenManager = require('./security/JwtTokenManager');
 const UserRepository = require('../Domains/users/UserRepository');
 const AuthenticationRepository = require('../Domains/authentications/AuthenticationRepository');
 const ThreadRepository = require('../Domains/threads/ThreadRepository');
+const CommentRepository = require('../Domains/comments/CommentRepository');
 
 // app helpers
 const PasswordHasher = require('../Applications/security/PasswordHasher');
@@ -29,6 +31,7 @@ const UserLoginUseCase = require('../Applications/use_case/UserLoginUseCase');
 const RefreshAuthenticationUseCase = require('../Applications/use_case/RefreshAuthenticationUseCase');
 const UserLogoutUseCase = require('../Applications/use_case/UserLogoutUseCase');
 const AddThreadUseCase = require('../Applications/use_case/AddThreadUseCase');
+const AddCommentUseCase = require('../Applications/use_case/AddCommentUseCase');
 
 // creating container
 const container = createContainer();
@@ -57,6 +60,16 @@ container.register([
   {
     key: ThreadRepository.name,
     Class: ThreadRepositoryPostgres,
+    parameter: {
+      dependencies: [
+        { concrete: pool },
+        { concrete: nanoid },
+      ],
+    },
+  },
+  {
+    key: CommentRepository.name,
+    Class: CommentRepositoryPostgres,
     parameter: {
       dependencies: [
         { concrete: pool },
@@ -138,6 +151,16 @@ container.register([
       injectType: 'destructuring',
       dependencies: [
         { name: 'threadRepository', internal: ThreadRepository.name },
+      ],
+    },
+  },
+  {
+    key: AddCommentUseCase.name,
+    Class: AddCommentUseCase,
+    parameter: {
+      injectType: 'destructuring',
+      dependencies: [
+        { name: 'commentRepository', internal: CommentRepository.name },
       ],
     },
   },
