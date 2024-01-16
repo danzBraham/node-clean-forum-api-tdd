f1d50b24b1fabd05eb687bd6a7af468247d39026
Zidan Abraham <zidangit07@gmail.com>
2024-01-16 15:27:08 +0800

    feat(domain): add new method in comment repository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -6,6 +6,10 @@ class CommentRepository {
   async verifyOwner(userId) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
+
+  async deleteComment(commentId) {
+    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
+  }
 }
 
 module.exports = CommentRepository;
