e0d146da8411646caa530e2353c0b9cb128bba71
Zidan Abraham <zidangit07@gmail.com>
2024-01-16 15:57:30 +0800

    feat(domain): remove verifyOwner method in comment repository

--- src/Domains/comments/CommentRepository.js
+++ src/Domains/comments/CommentRepository.js
@@ -3,10 +3,6 @@ class CommentRepository {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
 
-  async verifyOwner(userId) {
-    throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
-  }
-
   async deleteComment(commentId) {
     throw new Error('COMMENT_REPOSITORY.METHOD_NOT_IMPLEMENTED');
   }
