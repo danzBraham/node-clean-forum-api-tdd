e2d66a6089c618c75ce8aec05e91165824257622
Zidan Abraham <zidangit07@gmail.com>
2024-01-18 17:47:13 +0800

    feat(db): create table replies via migration

---
+++ migrations/1705570929081_create-table-replies.js
@@ -0,0 +1,36 @@
+/* eslint-disable camelcase */
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('replies', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    comment_id: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+    content: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    date: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    owner: {
+      type: 'VARCHAR(50)',
+      notNull: true,
+    },
+  });
+
+  pgm.addConstraint('replies', 'fk_replies.comment_id_comments.id', 'FOREIGN KEY(comment_id) REFERENCES comments(id) ON DELETE CASCADE');
+  pgm.addConstraint('replies', 'fk_replies.owner_users.id', 'FOREIGN KEY(owner) REFERENCES users(id) ON DELETE CASCADE');
+};
+
+exports.down = (pgm) => {
+  pgm.dropConsraint('replies', 'fk_replies.owner_users.id');
+  pgm.dropConsraint('replies', 'fk_replies.comment_id_comments.id');
+  pgm.dropTable('replies');
+};
