30d83a493246ed5f505aadd94d0a4cbb5ef02dfa
Zidan Abraham <zidangit07@gmail.com>
2024-01-19 10:30:52 +0800

    feat(infra/repo): add checkAvailabilityReply and deleteReply methods in ReplyRepositoryPostgres

--- src/Infrastructures/repository/ReplyRepositoryPostgres.js
+++ src/Infrastructures/repository/ReplyRepositoryPostgres.js
@@ -1,6 +1,9 @@
+const AuthorizationError = require('../../Commons/AuthorizationError');
+const NotFoundError = require('../../Commons/NotFoundError');
 const ReplyRepository = require('../../Domains/replies/ReplyRepository');
 const AddReply = require('../../Domains/replies/entities/AddReply');
 const AddedReply = require('../../Domains/replies/entities/AddedReply');
+const DeleteReply = require('../../Domains/replies/entities/DeleteReply');
 
 class ReplyRepositoryPostgres extends ReplyRepository {
   constructor(pool, idGenerator) {
@@ -21,6 +24,33 @@ class ReplyRepositoryPostgres extends ReplyRepository {
     const result = await this._pool.query(query);
 
     return new AddedReply({ ...result.rows[0] });
+  }
+
+  async checkAvailabilityReply(replyId) {
+    const query = {
+      text: 'SELECT id FROM replies WHERE id = $1',
+      values: [replyId],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new NotFoundError('reply not found');
+    }
+  }
+
+  async deleteReply(reply) {
+    const { userId: owner, commentId, replyId } = new DeleteReply(reply);
+
+    const query = {
+      text: `UPDATE replies SET is_deleted = true
+              WHERE owner = $1 AND comment_id = $2 AND id = $3`,
+      values: [owner, commentId, replyId],
+    };
+    const result = await this._pool.query(query);
+
+    if (!result.rowCount) {
+      throw new AuthorizationError('you cannot delete this reply');
+    }
   }
 }
 
--- src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
+++ src/Infrastructures/repository/_test/ReplyRepositoryPostgres.test.js
@@ -4,8 +4,11 @@ const CommentsTableTestHelper = require('../../../../tests/CommentsTableTestHelp
 const RepliesTableTestHelper = require('../../../../tests/RepliesTableTestHelper');
 const AddReply = require('../../../Domains/replies/entities/AddReply');
 const AddedReply = require('../../../Domains/replies/entities/AddedReply');
+const DeleteReply = require('../../../Domains/replies/entities/DeleteReply');
 const pool = require('../../database/postgres/pool');
 const ReplyRepositoryPostgres = require('../ReplyRepositoryPostgres');
+const NotFoundError = require('../../../Commons/NotFoundError');
+const AuthorizationError = require('../../../Commons/AuthorizationError');
 
 describe('ReplyRepositoryPostgres', () => {
   afterEach(async () => {
@@ -73,6 +76,115 @@ describe('ReplyRepositoryPostgres', () => {
 
       // Assert
       expect(addedReply).toStrictEqual(expectedAddedReply);
+    });
+  });
+
+  describe('checkAvailabilityReply function', () => {
+    it('should throw a NotFoundError if the reply is not found', async () => {
+      // Arrange
+      const replyId = 'reply-123';
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.checkAvailabilityReply(replyId))
+        .rejects
+        .toThrow(NotFoundError);
+    });
+
+    it('should not throw a NotFoundError if the reply is found', async () => {
+      // Arrange
+      const userId = 'user-123';
+      const threadId = 'thread-123';
+      const commentId = 'comment-123';
+      const replyId = 'reply-123';
+
+      await UsersTableTestHelper.addUser({ id: userId });
+      await ThreadsTableTestHelper.addThread({ id: threadId, owner: userId });
+      await CommentsTableTestHelper.addComment({ id: commentId, owner: userId });
+      await RepliesTableTestHelper.addReply({ id: replyId, owner: userId });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.checkAvailabilityReply(replyId))
+        .resolves
+        .not.toThrow(NotFoundError);
+    });
+  });
+
+  describe('deleteReply function', () => {
+    it('should delete a reply in the database correctly', async () => {
+      // Arrange
+      const threadId = 'thread-123';
+
+      const deleteReply = new DeleteReply({
+        userId: 'user-123',
+        commentId: 'comment-123',
+        replyId: 'reply-123',
+      });
+
+      await UsersTableTestHelper.addUser({
+        id: deleteReply.userId,
+      });
+      await ThreadsTableTestHelper.addThread({
+        id: threadId,
+        owner: deleteReply.userId,
+      });
+      await CommentsTableTestHelper.addComment({
+        id: deleteReply.commentId,
+        threadId,
+        owner: deleteReply.userId,
+      });
+      await RepliesTableTestHelper.addReply({
+        id: deleteReply.replyId,
+        commentId: deleteReply.commentId,
+        owner: deleteReply.userId,
+      });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action
+      await replyRepositoryPostgres.deleteReply(deleteReply);
+
+      // Assert
+      const replies = await RepliesTableTestHelper.findReplyById('reply-123');
+      expect(replies[0].is_deleted).toEqual(true);
+    });
+
+    it("should throw an AuthorizationError if trying to delete someone else's reply", async () => {
+      // Arrange
+      const threadId = 'thread-123';
+
+      const deleteReply = new DeleteReply({
+        userId: 'user-123',
+        commentId: 'comment-123',
+        replyId: 'reply-123',
+      });
+
+      await UsersTableTestHelper.addUser({
+        id: 'user-456',
+      });
+      await ThreadsTableTestHelper.addThread({
+        id: threadId,
+        owner: 'user-456',
+      });
+      await CommentsTableTestHelper.addComment({
+        id: deleteReply.commentId,
+        threadId,
+        owner: 'user-456',
+      });
+      await RepliesTableTestHelper.addReply({
+        id: deleteReply.replyId,
+        commentId: deleteReply.commentId,
+        owner: 'user-456',
+      });
+
+      const replyRepositoryPostgres = new ReplyRepositoryPostgres(pool, {});
+
+      // Action & Assert
+      await expect(replyRepositoryPostgres.deleteReply(deleteReply))
+        .rejects
+        .toThrow(AuthorizationError);
     });
   });
 });
